0fb0d90be6a51ee91e88c4df0ec930919ff75e68
Chika <djchikalkin@gmail.com>
2021-07-05 18:04:41 +0300

    clean code

--- handlers/message.js
+++ handlers/message.js
@@ -1,10 +1,7 @@
 const { ok } = require('../config/phrases.json');
 
 module.exports = ctx => {
-  if (
-    ctx.message.reply_to_message &&
-    ctx.message.reply_to_message.from.is_bot === true
-  ) {
+  if (ctx?.message?.reply_to_message?.from.is_bot) {
     ctx.reply(ok);
   }
 };
